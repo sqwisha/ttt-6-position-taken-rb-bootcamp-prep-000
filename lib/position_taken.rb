8c5624b52aa3dd58be338116ba4df0249d7b0aa0
S H <sashanicolehart@gmail.com>
2018-09-09 23:52:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
-def position_taken?(array, input)
+def position_taken?(array, input, character = "X")
   index = input.to_i - 1
   if array[index] == ""
-    array[index] = ' #{input} '
+    array[index] = " #{character} "
   else
-    
+    puts ""
   end  
 end  
