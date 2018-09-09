189645d46c3040366c613f92971bc6564ce3e52f
S H <sashanicolehart@gmail.com>
2018-09-09 23:51:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
-def position_taken?(arr[i])
-  if arr[]
+def position_taken?(array, input)
+  index = input.to_i - 1
+  if array[index] != ""
+    array[index] = ' #{input} '
+  else
   end  
 end  
