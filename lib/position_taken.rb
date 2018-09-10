528e93cd9e652a84f28987f7f406ed4f29cfe4f1
S H <sashanicolehart@gmail.com>
2018-09-10 00:00:57 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(array, input)
   index = input.to_i - 1
-  if array[index] == "" || " "
+  if array[index].value == "" || " "
     return false
-  elsif array[index] == "X" || "O"
+  elsif array[index].value == "X" || "O"
     return true
   else
     return true
