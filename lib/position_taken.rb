184029aec89fb83dc026c4673471e096a4b47ca9
S H <sashanicolehart@gmail.com>
2018-09-10 00:31:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if board[index].strip.empty|| board[index] == nil   
+  if board[index].strip.empty || board[index] == nil   
     return false
   elsif board[index] == "X" || board[index] == "O"
     return true
