3e90b13af8015cfe761ce59077df94137313473b
S H <sashanicolehart@gmail.com>
2018-09-10 00:22:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
     return false
   elsif index == nil
     return false
-  else if board[index].include? "X" || "O"
+  else if board[index] == 'X" || "O"
     return true
   end  
 end
