13db0cc63990834620a9512af2819ec5542629e3
S H <sashanicolehart@gmail.com>
2018-09-10 00:20:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,3 +5,15 @@ def position_taken?(board, index)
     return false
   end  
 end  
+
+def position_taken?(board, index)
+  if (board[index] == " "
+    return false
+  elsif board[index] == " "
+    return false
+  elsif index == nil
+    return false
+  else if board[index].include? "X" || "O"
+    return true
+  end  
+end  
